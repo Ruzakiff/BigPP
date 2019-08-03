4fbe0c00e02307668db28a364e4d6823e0a1bfc4
Ruzakiff <ryanchenyang@gmail.com>
2019-08-03 15:14:26 -0400

    added hands functions

--- Hands.py
+++ Hands.py
@@ -11,3 +11,23 @@ def fold():
         pyautogui.click(x=clickx,y=clicky)
         return True
     return False
+
+def call():
+    if findImage('/Users/ryan/Desktop/bigPP/Assets/Call.png',.85):
+        print("Fold Image Found")
+        clickx=eyes.clickTarget[0]/2
+        clicky=eyes.clickTarget[1]/2
+        print("Folding")
+        pyautogui.click(x=clickx,y=clicky)
+        return True
+    return False
+
+def check():
+    if findImage('/Users/ryan/Desktop/bigPP/Assets/Check.png',.85):
+        print("Fold Image Found")
+        clickx=eyes.clickTarget[0]/2
+        clicky=eyes.clickTarget[1]/2
+        print("Folding")
+        pyautogui.click(x=clickx,y=clicky)
+        return True
+    return False
